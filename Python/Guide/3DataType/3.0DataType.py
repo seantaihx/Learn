263dc5d7d6a5c282e89968d53b1a1b53dedf9533
seantaihx <168383056+seantaihx@users.noreply.github.com>
2025-05-22 15:34:50 +0000

    Data type

--- Python/Guide/3DataType/3.0DataType.py
+++ Python/Guide/3DataType/3.0DataType.py
@@ -1,5 +1,53 @@
 #Numeric Data Types
 '''
-1. Numbers
+Numbers: Numbers without quotation marks
 
+1. Integer: Whole number without decimals
+ex: 1, 56, 48723278427
+
+2. Float: Numbers with decimals
+ex: 1.0, 423.2143, 213.3413
+
+3. Complex: Numbers with real part and imaginary part
+ex: 3+2j, 1+2j
+'''
+
+
+
+#String
+'''
+String: Everything inside quotation marks, double or single is fine
+ex: '1', "ufwfueni", "342"
+
+!!!Everything from input is string
+'''
+
+
+
+#Boolean
+'''
+Boolean: True or False
+ex: True, False
+
+!!Note the capital letter
+'''
+
+
+
+#Data Type Checking
+'''
+type(): Check the type of a value
+
+format: type(value)             --> This is not printed, so we usually accompany this with print()
+ex: print(type(1))
+'''
+
+
+
+#Data Type Conversion
+'''
+int(): change to integer
+float(): change to float
+str(): change to string
+bool(): change to boolean
 '''
---
+++ Python/Guide/3DataType/3.1Practice.py
@@ -0,0 +1,42 @@
+a = 1
+b = 1.5
+c = 1 + 2j
+d = "4"
+e = "True"
+f = True
+
+print(type(a))
+a = str(a)
+print(type(a))
+print('\n')
+
+
+print(type(b))
+b = int(b)
+print(b)
+print(type(b))
+print('\n')
+
+
+print(type(c))
+c = str(c)
+print(type(c))
+print('\n')
+
+
+print(type(d))
+d = int(d)
+print(type(d))
+print('\n')
+
+
+print(type(e))
+e = bool(e)
+print(type(e))
+print('\n')
+
+
+print(type(f))
+f = str(f)
+print(type(f))
+print('\n')
