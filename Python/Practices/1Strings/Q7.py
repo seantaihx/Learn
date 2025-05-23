2db0c0c11a80f820981ccc277148b347779dcc03
seantaihx <168383056+seantaihx@users.noreply.github.com>
2025-05-23 16:32:47 +0000

    Small bug

--- Python/Practices/1Strings/Q7.py
+++ Python/Practices/1Strings/Q7.py
@@ -7,5 +7,17 @@ Check if it reads the same forward and backward (e.g. “madam”).
 
 '''
 Tips:
-1. 
+1. two ways, one is check the character one by one, one is check the whole word
+2. slicing for second way
+'''
+
+
+'''Pseudocode 1
+1. Get input and create a for loop iterate over the word
+2. check if the i forward is equal to the respective character backward
+'''
+
+'''Pseudocode 2
+1. Get input
+2. Check if word is equal to the word spelled backward
 '''
