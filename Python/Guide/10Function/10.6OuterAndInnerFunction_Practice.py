bcc8d0075ba1ab381a1e224d802b2eecd8a2bf70
seantaihx <168383056+seantaihx@users.noreply.github.com>
2025-05-24 16:54:16 -0400

    Create 10.6OuterAndInnerFunction_Practice.py

---
+++ Python/Guide/10Function/10.6OuterAndInnerFunction_Practice.py
@@ -0,0 +1,18 @@
+def outer(name):
+    def inner():
+        return "Hello, " + name
+    return inner()
+
+greeting = outer("Alice")
+print(greeting)  
+
+
+'''Explanation
+outer(name)
+This is the outer function which takes one argument: name.
+
+inner()
+This is an inner function, defined inside outer() and has access to the name variable from the outer function.
+
+return inner()
+The outer function calls and returns the result of the inner function.
