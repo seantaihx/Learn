f18583ec584d4407f89ec080bc832a4d9fa861ec
seantaihx <168383056+seantaihx@users.noreply.github.com>
2025-05-24 14:30:26 -0400

    Create 8.8SummaryCollection.py

---
+++ Python/Guide/08Collections/8.8SummaryCollection.py
@@ -0,0 +1,9 @@
+#In Summary
+'''
+List when we need to modify the value multiple times
+Tuple when the value should NOT be changed
+Set when want no duplicates, or anything regarding duplicates
+Dictionary when want a key value pair
+
+To change a tuple, change it into list and modify, then change back to tuple
+'''
