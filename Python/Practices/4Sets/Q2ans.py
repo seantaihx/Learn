bc37d8cc75ce528c152f5147e2fcdb83144648e1
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:30:01 -0400

    Create Q2ans.py

---
+++ Python/Practices/4Sets/Q2ans.py
@@ -0,0 +1,12 @@
+'''
+Given two sets:
+set1 = {1, 2, 3, 4}  
+set2 = {3, 4, 5, 6}
+	•	Print their intersection, union, and difference (set1 - set2).
+'''
+
+set1 = {1, 2, 3, 4}  
+set2 = {3, 4, 5, 6}
+print(set1.intersection(set2))
+print(set1.union(set2))
+print(set1-set2)
