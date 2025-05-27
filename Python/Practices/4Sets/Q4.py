bc8be09374e620a761378bbdcf5d65cc677f7779
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:34:09 -0400

    Create Q4.py

---
+++ Python/Practices/4Sets/Q4.py
@@ -0,0 +1,7 @@
+'''
+Given a list with duplicates:
+data = [1, 2, 2, 3, 4, 4, 5]
+	•	Convert it to a set to remove duplicates.
+	•	Convert it back to a list.
+	•	Print both the new list and its length.
+'''
