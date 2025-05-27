0bcea7fd979ac8a5416a836d371f1c73f9ef4a97
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-26 21:34:44 -0400

    Create 14.0Module.py

---
+++ Python/Guide/14Module/14.0Module.py
@@ -0,0 +1,23 @@
+#Module
+'''
+Module: Code library
+'''
+
+#Naming Module
+'''
+We can name a file(Our Module) whatever we want as long as its file extension is .py
+'''
+
+#Using A Module
+'''
+format: 
+import module_name1 as name_you_prefer, module_name2, module_name3...
+
+to use a specific function
+format: module_name.function_name(function_argument)
+'''
+
+#Import Part of Module
+'''
+format: from module_name import function/variable
+'''
