0af70f29c61e7fc8db828710bb5c28c73c00f577
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:33:16 -0400

    Create Q3.py

---
+++ Python/Practices/4Sets/Q3.py
@@ -0,0 +1,8 @@
+'''
+Start with this set:
+nums = {10, 20, 30}
+	•	Add multiple elements using update().
+	•	Then remove an element using discard() (choose an element that doesn’t exist to test it).
+	•	Try using remove() on a missing element and see what happens.
+'''
+
