9007c27867df4521c631ac545336cac20fae32de
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:22:48 -0400

    Create Q4.py

---
+++ Python/Practices/3Tuples/Q4.py
@@ -0,0 +1,6 @@
+'''
+You have the tuple:
+fruits = ('apple', 'banana', 'cherry')
+	•	Use enumerate() to print each item with its position in the format:
+    1: apple, 2: banana, etc. (Start count at 1)
+'''
