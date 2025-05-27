5fc234b07d1638f11e88d731343411125773a51c
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:27:26 -0400

    Create Q1ans.py

---
+++ Python/Practices/4Sets/Q1ans.py
@@ -0,0 +1,14 @@
+'''
+Create a set with duplicate elements, then print the set.
+	•	Explain what happened to the duplicates.
+	•	Add a new element to the set.
+'''
+
+mysets = {1, 1, 2, 3, 4, 4, 5, 6, 6}
+mysets.add(7)
+print(mysets)
+
+
+'''SAMPLE OUTPUT
+{1, 2, 3, 4, 5, 6, 7}
+'''
