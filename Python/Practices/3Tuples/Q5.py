d9ad14e133ae85f1fef74f94053a06be788c2ece
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:24:03 -0400

    Create Q5.py

---
+++ Python/Practices/3Tuples/Q5.py
@@ -0,0 +1,7 @@
+'''
+Use tuples as keys in a dictionary:
+	•	Create a dictionary where the key is a (latitude, longitude) tuple and the value is a city name.
+	•	Add three locations.
+	•	Print all items in the format:
+"City: [name], Coordinates: ([lat], [long])"
+'''
