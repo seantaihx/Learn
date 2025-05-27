960956d9a5618e63278a955fdc6b3332ae1ae44e
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-27 18:39:18 -0400

    Create Q5ans.py

---
+++ Python/Practices/4Sets/Q5ans.py
@@ -0,0 +1,16 @@
+'''
+Create a set:
+items = {"pen", "pencil", "eraser"}
+	•	Check if "pen" is in the set.
+	•	Clear the set using a method.
+	•	Then check if the set is empty.
+'''
+
+items = {"pen", "pencil", "eraser"}
+
+
+print("pen" in items)  
+items.clear()
+
+print(len(items) == 0)  
+print(items)            
