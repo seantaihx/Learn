7400f75fa9cb7adc839407b59078f40395614575
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:20:08 -0400

    Update Q2ans.py

--- Python/Practices/7Class_StaticMethod/Q2ans.py
+++ Python/Practices/7Class_StaticMethod/Q2ans.py
@@ -2,3 +2,14 @@
 Create a class User with a class variable user_count.
 	•	Add a @classmethod called create_guest() that returns a user with name "Guest" and increases user_count.
  '''
+
+class User:
+    user_count = 0  # Class variable to track number of users
+
+    def __init__(self, name):
+        self.name = name
+        User.user_count += 1
+
+    @classmethod
+    def create_guest(cls):
+        return cls("Guest")
