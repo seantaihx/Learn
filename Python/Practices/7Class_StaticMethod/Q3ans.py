e8bf505375f1a6dddf2f9b13f3f9252ca03e9a5f
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:20:43 -0400

    Update Q3ans.py

--- Python/Practices/7Class_StaticMethod/Q3ans.py
+++ Python/Practices/7Class_StaticMethod/Q3ans.py
@@ -3,3 +3,11 @@ Create a base class Animal with a method speak().
 	•	Create a subclass Dog that overrides speak() to print "Woof!".
 	•	Create a Dog object and call speak().
 '''
+
+class Animal:
+    def speak(self):
+        print("Animal sound")
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
