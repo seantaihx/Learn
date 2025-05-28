fc8ee6ac50e3a882af2895b3b0973a3bd63302ef
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:01:08 -0400

    Create Q5.py

---
+++ Python/Practices/6Class/Q5.py
@@ -0,0 +1,5 @@
+'''
+Add a class variable called count to track how many Book objects have been created.
+	•	Each time a new book is created, increase count by 1.
+	•	Print Book.count after making 3 books.
+'''
