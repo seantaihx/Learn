6245ea55f25d5c8a9b5325f0986e6dced74db7fe
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:19:44 -0400

    Create Q5ans.py

---
+++ Python/Practices/7Class_StaticMethod/Q5ans.py
@@ -0,0 +1,4 @@
+'''
+Create a base class Employee with a method info() that prints "Base employee info".
+	•	Create a subclass Manager that overrides info() but also calls super().info() before printing "Manager info".
+'''
