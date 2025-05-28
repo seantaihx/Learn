7988d542ebfd83926e6b2f04781a8f181b21b481
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 13:19:36 -0400

    Create Q2.py

---
+++ Python/Practices/5Function/Q2.py
@@ -0,0 +1,5 @@
+'''
+Write a function get_item(lst, index) that returns the element at the given index of a list.
+	•	Handle IndexError if the index is out of range.
+	•	Return "Invalid index" if an error occurs.
+'''
