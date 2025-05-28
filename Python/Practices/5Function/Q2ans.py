3ea3b3ab6c418aa1c4373566c9fc3e07c05dbe5e
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 13:22:23 -0400

    Create Q2ans.py

---
+++ Python/Practices/5Function/Q2ans.py
@@ -0,0 +1,19 @@
+'''
+Write a function get_item(lst, index) that returns the element at the given index of a list.
+	•	Handle IndexError if the index is out of range.
+	•	Return "Invalid index" if an error occurs.
+'''
+
+def get_item(lst, index):
+  try:
+    return lst[index]
+  except IndexError:
+    return "Invalid index"
+
+
+def main():
+  print(get_item([1, 2, 3], 1))   # 2
+  print(get_item([1, 2, 3], 5))   # Invalid index
+
+if __name__: '__main__':
+  main()
