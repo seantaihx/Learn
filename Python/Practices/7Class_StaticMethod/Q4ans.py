c5155c7ca1ca603aabcdfe317b875bcb384a238d
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:21:27 -0400

    Update Q4ans.py

--- Python/Practices/7Class_StaticMethod/Q4ans.py
+++ Python/Practices/7Class_StaticMethod/Q4ans.py
@@ -3,3 +3,23 @@ Create a class Circle with a private attribute _radius.
 	•	Use @property to get the radius and compute the area.
 	•	Use @radius.setter to safely update _radius.
  '''
+
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius  # Private-like attribute
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value < 0:
+            print("Radius cannot be negative.")
+        else:
+            self._radius = value
+
+    @property
+    def area(self):
+        return 3.1416 * (self._radius ** 2)
