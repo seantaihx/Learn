74cbbc119b8a406c669704f9ce3c3c11b3682aa4
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:22:24 -0400

    Update Q5ans.py

--- Python/Practices/7Class_StaticMethod/Q5ans.py
+++ Python/Practices/7Class_StaticMethod/Q5ans.py
@@ -2,3 +2,13 @@
 Create a base class Employee with a method info() that prints "Base employee info".
 	•	Create a subclass Manager that overrides info() but also calls super().info() before printing "Manager info".
 '''
+
+
+class Employee:
+    def info(self):
+        print("Base employee info")
+
+class Manager(Employee):
+    def info(self):
+        super().info()  # Call the parent method first
+        print("Manager info")
