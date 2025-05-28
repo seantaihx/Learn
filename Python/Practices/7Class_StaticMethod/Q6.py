29c876ffeee5ac160cf9be07200efe3b8dd65faa
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-05-28 15:23:07 -0400

    Create Q6.py

---
+++ Python/Practices/7Class_StaticMethod/Q6.py
@@ -0,0 +1,10 @@
+'''
+Create an abstract base class Shape with an abstract method area().
+	•	Create two subclasses: RectangleMixin (with width and height) and PrintableMixin (adds a __repr__() that shows the shape’s dimensions).
+	•	Then, create a class PrintableRectangle that inherits from both RectangleMixin and PrintableMixin, and also inherits from Shape.
+
+Finally:
+	•	Implement the area() method in PrintableRectangle.
+	•	Create an instance of PrintableRectangle, print it, and print its area.
+ '''
+
