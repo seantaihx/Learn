854d97ceedd15a123704bdb9dde85549594ecc2f
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-06-03 04:38:22 -0400

    Create Q2ans.py

---
+++ Python/Practices/8Iterators/Q2ans.py
@@ -0,0 +1,24 @@
+'''
+class OneToN:
+    def __init__(self, n):
+        self.n = n
+        self.current = 1
+
+    def __iter__(self):
+        return self
+
+    def ____________(self):  # complete this method
+        if self.current > self.n:
+            raise ____________
+        val = self.current
+        self.current += 1
+        return val
+
+it = OneToN(3)
+print(next(it))
+print(next(it))
+print(next(it))
+
+Fill in the blanks so that this class works like an iterator.
+'''
+
