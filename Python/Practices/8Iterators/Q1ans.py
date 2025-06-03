ecaf8af9c2546288eabcf7b0da1df68780b8e69f
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-06-03 04:37:08 -0400

    Create Q1ans.py

---
+++ Python/Practices/8Iterators/Q1ans.py
@@ -0,0 +1,13 @@
+'''
+nums = [100, 200, 300]
+it = ______(nums)
+print(______)
+print(______)
+
+Fill in the blanks to print the first two elements of the list using an iterator.
+'''
+
+nums = [100, 200, 300]
+it = iter(nums)
+print(next(it))
+print(next(it))
