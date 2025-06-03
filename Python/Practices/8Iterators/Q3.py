0f2e84edb4d840816421f7501e91ba7b32ab683e
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-06-03 04:39:15 -0400

    Create Q3.py

---
+++ Python/Practices/8Iterators/Q3.py
@@ -0,0 +1,11 @@
+'''
+def even_up_to_10():
+    for i in range(11):
+        if ______:
+            yield ______
+
+for num in even_up_to_10():
+    print(num)
+    
+Complete the function to only yield even numbers from 0 to 10.
+'''
