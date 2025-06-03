a71a2813343f232be09b413460e8aaa137fa6ce2
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-06-03 04:46:51 -0400

    Update Q4ans.py

--- Python/Practices/8Iterators/Q4ans.py
+++ Python/Practices/8Iterators/Q4ans.py
@@ -16,3 +16,16 @@ for j in obj:
 Complete __iter__ so both loops print 10, 20. (Hint: You must return a new iterator each time.)
 '''
 
+class ResetOnIter:
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        return iter(self.data)  # return a new iterator object each time
+
+# Use case
+obj = ResetOnIter([10, 20])
+for i in obj:
+    print(i)
+for j in obj:
+    print(j)
