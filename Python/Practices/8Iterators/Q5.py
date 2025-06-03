586a213473933d6abe1059b5e6f57a8481b85d74
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-06-03 04:40:58 -0400

    Create Q5.py

---
+++ Python/Practices/8Iterators/Q5.py
@@ -0,0 +1,24 @@
+'''
+class FibUpTo:
+    def __init__(self, max_value):
+        self.max = max_value
+        self.a = 0
+        self.b = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.a > self.max:
+            raise StopIteration
+        val = self.a
+        self.a, self.b = ________
+        return val
+
+# Output: 0 1 1 2 3 5 8 13 21
+for num in FibUpTo(21):
+    print(num)
+
+Complete the missing line to make the Fibonacci logic correct.
+'''
+
