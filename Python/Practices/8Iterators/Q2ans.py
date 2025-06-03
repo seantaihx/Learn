025bc94eb728d1daa46b09470be31a2f13c5884b
Sean Tai <168383056+seantaihx@users.noreply.github.com>
2025-06-03 04:41:32 -0400

    Update Q2ans.py

--- Python/Practices/8Iterators/Q2ans.py
+++ Python/Practices/8Iterators/Q2ans.py
@@ -22,3 +22,22 @@ print(next(it))
 Fill in the blanks so that this class works like an iterator.
 '''
 
+class OneToN:
+    def __init__(self, n):
+        self.n = n
+        self.current = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):  # complete this method
+        if self.current > self.n:
+            raise StopIteration
+        val = self.current
+        self.current += 1
+        return val
+
+it = OneToN(3)
+print(next(it))
+print(next(it))
+print(next(it))
